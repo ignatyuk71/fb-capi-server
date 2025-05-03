3df0a331942979e848e3beae42e4cf87ce1b9854
Mykhailo <ignatyuk71@gmail.com>
2025-05-03 02:19:50 +0200

    Fix Railway deployment config cros

--- index.js
+++ index.js
@@ -3,35 +3,32 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Використовуємо middleware для парсингу JSON
+// Middleware для парсингу JSON
 app.use(express.json());
 
-// Додаємо CORS, щоб дозволити запити з вашого сайту
+// Налаштування CORS
 const corsOptions = {
-  origin: 'https://dream-v-doma.tilda.ws', // Дозволяємо доступ тільки з вашого домену
-  methods: ['GET', 'POST'], // Дозволяємо лише GET та POST запити
-  allowedHeaders: ['Content-Type', 'Authorization'], // Дозволяємо лише певні заголовки
+  origin: 'https://dream-v-doma.tilda.ws', // Замість цього вказуйте свій сайт
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
 
-
-
-const PIXEL_ID = '1667929657386446';
-const ACCESS_TOKEN = 'EAAHpt1ZAxmGMBO1ZCZCQxb2moZBha8p6P6ZB7audYL3OgM9i81xU0lvZAC5SRXewVhpno24CMnVIYevh3UYuSvzlBL8J8lWxyu203UksuchowA0lSI5X8LZAkU4vvbAKa8jZBZAKXwAQkkTLtq0mRQWIkDB1gBCfLlpOE9VbgTXTlhNxV7WvsQFHR492XtDLMbiZBaFgZDZD';
-
-
 // Маршрут для обробки POST запитів
-app.post('/api/pageView11', (req, res) => {
-  const data = req.body; // Дані, які прийшли з запиту
+app.post('/api', (req, res) => {
+  const data = req.body;
   console.log(data);
 
-
-  
   // Відповідь на запит
   res.status(200).json({
     status: 'success',
     message: 'POST request received',
     data: data,
   });
+});
+
+// Старт сервера
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
